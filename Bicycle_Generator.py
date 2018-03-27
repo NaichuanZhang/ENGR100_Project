fd6f9c78b5ad1631642a4c76633b40d816b873a4
alex <naichuan@umich.edu>
2018-03-26 22:31:10 -0400

    simple helper

--- Bicycle_Generator.py
+++ Bicycle_Generator.py
@@ -8,18 +8,16 @@ class Bicycle_Generator:
         """
         Structure for data_dict
         {
-            bicycle_weight:int kg,
-            rider_weight: int kg,
-            bicycle_wheel_diameter: int m
-            bicycle_pedal_diameter: int m
-            bicycle_front_gear_diameter: int m
-            bicycle_back_gear_diameter: int m
+            bicycle_weight: double kg,
+            rider_weight: double kg,
+            bicycle_wheel_diameter: double m
+            bicycle_pedal_diameter: double m
+            bicycle_front_gear_diameter: double m
+            bicycle_back_gear_diameter: double m
             coil_turns: int
-            coil_magnet_distance: int m
-            bulb_voltage: int V
-            circuit_resistance: int ohm
-            
-            
+            coil_magnet_distance: double m
+            bulb_voltage: double V
+            circuit_resistance: double ohm
         }
 
         Thing to calculate:
@@ -40,11 +38,32 @@ class Bicycle_Generator:
         self.circuit_resistance = data_dict['circuit_resistance']
         
         # Assumption Parameter 
-        self.pedal_force = 300 # Unit N
-        self.pedal_distance = 0.15 m
+        self.pedal_force = 300.0 # Unit N
+        self.pedal_distance = 0.175 # m
+        self.bike_speed = 7.0 # m/s
         
+        # Common constant
+        pi = 3.14 
         
         def force_transfer(self):
-            front_gear_distance = 
+            front_gear_distance = self.bicycle_front_gear_diameter / self.bicycle_pedal_diameter * self.pedal_distance
+            back_gear_distance = self.bicycle_back_gear_diameter / self.bicycle_back_gear_diameter * front_gear_distance
+            wheel_distance = self.bicycle_wheel_diameter / self.bicycle_back_gear_diameter * back_gear_distance
+            print ("pedal_distance: ", self.pedal_distance, " wheel_distance: ", wheel_distance)
+            torque_input = self.pedal_force * self.pedal_distance
+            torque_back_gear = self.bicycle_back_gear_diameter / self.bicycle_back_gear_diameter * torque_input 
+            print ("torque input: ", torque_input, " torque_back_gear: ", torque_back_gear)
+            return 
+
+        def power_output(self):
+            pedal_velocity = self.bicycle_pedal_diameter / self.bicycle_wheel_diameter * bike_speed
+            print('pedal velocity: ', pedal_velocity)
+            power = self.pedal_force * pedal_velocity
+            print('pedal power: ', pedal_velocity)
+            return
+        
+        def Other_functions
+            return
+            
             
 
